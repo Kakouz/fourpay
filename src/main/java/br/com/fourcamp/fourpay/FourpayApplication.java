30273e5d95328a510e61f88a55f0f5a1c570992a
Tiago Prudente <tf.prudente@gmail.com>
2022-06-10 11:51:27 -0300

    Initial commit with the barebones of the project

---
+++ src/main/java/br/com/fourcamp/fourpay/FourpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.fourcamp.fourpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fourcamp/fourpay/FourpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fourcamp.fourpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
